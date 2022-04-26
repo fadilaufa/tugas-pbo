2d84d9677ee42f521c6101916b0dda661e6b5cc3
fadilaufa <83756786+fadilaufa@users.noreply.github.com>
2022-04-26 22:28:21 +0700

    Update pertama makanan.java
    membuat inheritance dari menu.java
    menambahkan :
    1. method tampilTopping
    2. override method setHarga
    3. method tampil pesanan

--- makanan.java
+++ makanan.java
@@ -1,6 +1,6 @@
-public class makanan{
+public class makanan extends menu{
     private String topping;
-    private String[]daftar_topping = {"Sosis", "Telur", "Keju"};
+    private String[]daftar_topping = {"sosis", "telur", "keju"};
     private int []harga_topping = {3000, 1500, 3000};
     
     public void setTopping(String topping){
@@ -13,8 +13,8 @@ public class makanan{
     
     public int getHargatopping(){
         int harga = 0;
-        for (int i = 0; i< daftar_topping.length; i++){
-            if (daftar_topping[i] == topping) {
+        for (int i = 0; i < 3; i++){
+            if (daftar_topping[i].equals(getTopping())) {
                 harga = harga_topping[i];
                 break;
             }
@@ -22,4 +22,28 @@ public class makanan{
         return harga;
     }
     
+    public void tampilTopping() {
+        System.out.println("=== Daftar Topping ===");
+        for (int i = 0; i < daftar_topping.length; i++){
+            System.out.println((i+1) + ". " + daftar_topping[i] + "\t| Rp. " + harga_topping[i]);
+        }
+    }
+    
+    @Override
+    protected void setHarga(double harga){
+       double harga_akhir = harga;
+       double harga_topping = getHargatopping();
+    
+       harga_akhir = harga_akhir + harga_topping;   
+       harga_akhir = harga_akhir * super.getJumlah();
+       
+       super.harga = harga_akhir;
+    }
+    
+    public void tampil_pesanan(){
+       System.out.println("Nama = " + getNama());
+       System.out.println("Topping = " + getTopping() + " | Rp. " + getHargatopping());
+       System.out.println("Quantity = " + getJumlah());
+       System.out.println("Price = " + getHarga());
+    }
 }
