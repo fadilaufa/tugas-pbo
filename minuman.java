c606e9711800c68683eff10592430523932210d6
fadilaufa <83756786+fadilaufa@users.noreply.github.com>
2022-05-09 21:52:35 +0700

    mengubah mekanisme minuman.java
    mengubah setNama dan setHarga
    menambah daftar_minuman dan daftar_harga

--- minuman.java
+++ minuman.java
@@ -1,6 +1,21 @@
 public class minuman extends menu{
     private String ukuran;
     private String[]daftar_ukuran = {"Small", "Medium", "Large"};
+    private String[] daftar_minuman = {"Capuccino", "Kopi Susu", "Espresso", "Latte", "Milkshake Vanilla", "Milkshake Coklat"};
+    private String[]daftar_ukuran = {"Small", "Medium", "Large"};
+    
+    @Override
+    protected void setNama(){
+        int pil;
+        System.out.println("=== Daftar Menu ===");
+        for (int i = 0; i < daftar_minuman.length; i++){
+            System.out.println(i+1 + ". " + daftar_minuman[i] + "\t| Rp. " + daftar_harga[i]);
+        }
+        System.out.print("Masukkan Pilihan : ");
+        pil = input.nextInt();
+        this.id = pil - 1;
+        this.nama = daftar_minuman[pil-1];
+    }
     
     public void setUkuran(String ukuran){
         this.ukuran = ukuran;
@@ -24,8 +39,8 @@ public class minuman extends menu{
     }
     
     @Override
-    protected void setHarga(double harga){
-        double harga_akhir = harga;
+    protected void setHarga(){
+        double harga_akhir = this.daftar_harga[id];
         double harga_ukuran;
         harga_ukuran = harga_akhir * 15/100;
         if (daftar_ukuran[0].equals(getUkuran())){
