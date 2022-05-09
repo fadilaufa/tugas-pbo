59940adbf8dbbb4fde82cd19f850cc89b62833f2
fadilaufa <83756786+fadilaufa@users.noreply.github.com>
2022-05-09 21:47:36 +0700

    mengubah mekanisme class makanan
    menambahkan daftar makanan dan mengubah mekanisme setNama dan setHarga

--- makanan.java
+++ makanan.java
@@ -1,7 +1,22 @@
 public class makanan extends menu{
     private String topping;
+    private String[] daftar_makanan = {"Roti Bakar", "Mie Goreng", "Mie Rebus", "Pisang Crispy", "French Fries", "Sosis"};
+    private double[] daftar_harga = {10000, 12000, 12000, 15000, 15000, 15000};
     private String[]daftar_topping = {"sosis", "telur", "keju"};
     private int []harga_topping = {3000, 1500, 3000};
+    
+     @Override
+    protected void setNama(){
+        int pil;
+        System.out.println("=== Daftar Menu ===");
+        for (int i = 0; i < daftar_makanan.length; i++){
+            System.out.println(i+1 + ". " + daftar_makanan[i] + "\t| Rp. " + daftar_harga[i]);
+        }
+        System.out.print("Masukkan Pilihan : ");
+        pil = input.nextInt();
+        this.id = pil - 1;
+        this.nama = daftar_makanan[pil-1];
+    }
     
     public void setTopping(String topping){
         this.topping = topping;
@@ -30,8 +45,8 @@ public class makanan extends menu{
     }
     
     @Override
-    protected void setHarga(double harga){
-       double harga_akhir = harga;
+    protected void setHarga(){
+       double harga_akhir = daftar_harga[id];
        double harga_topping = getHargatopping();
     
        harga_akhir = harga_akhir + harga_topping;   
